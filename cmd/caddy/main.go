0bf4080496517bcc3aefd910d1bb455915cab05d
oluceps <secinia@outlook.com>
2024-10-15 06:01:05 +0800

    + porkbun acme

--- cmd/caddy/main.go
+++ cmd/caddy/main.go
@@ -37,6 +37,7 @@ import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 
 	// plug in Caddy modules here
+	_ "github.com/caddy-dns/porkbun"
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
 	_ "github.com/mholt/caddy-ratelimit"
 )
